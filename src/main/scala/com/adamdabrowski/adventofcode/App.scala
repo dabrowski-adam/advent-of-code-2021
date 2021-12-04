227b0f71fcff6a90cd080b98254c8c617b05b613
adam <contact@adamdabrowski.com>
2021-12-04 21:39:02 +0100

    Solve 2nd part of day 1

--- src/main/scala/com/adamdabrowski/adventofcode/App.scala
+++ src/main/scala/com/adamdabrowski/adventofcode/App.scala
@@ -1,8 +1,10 @@
 package com.adamdabrowski.adventofcode
 
-import com.adamdabrowski.adventofcode.day1.solve
+import com.adamdabrowski.adventofcode.day1.{solve, solveHalfway}
 
 import scala.io.Source
 
-@main def run(): Unit =
+@main def run(): Unit = {
+  println(solveHalfway(Source.fromResource("day1/input.txt")))
   println(solve(Source.fromResource("day1/input.txt")))
+}
--- src/main/scala/com/adamdabrowski/adventofcode/day1/Day1.scala
+++ src/main/scala/com/adamdabrowski/adventofcode/day1/Day1.scala
@@ -2,8 +2,15 @@ package com.adamdabrowski.adventofcode.day1
 
 import scala.io.Source
 
+// TODO: Switch out Source for something immutable
 val parseInput: Source => List[Int] =
   _.getLines.map(_.toInt).toList
+
+val sumTriplets: List[Int] => List[Int] =
+  _
+    .sliding(3, 1)
+    .map(_.sum)
+    .toList
 
 val countDepthIncreases: List[Int] => Int =
   _
@@ -12,5 +19,8 @@ val countDepthIncreases: List[Int] => Int =
     .map({ case List(x, y) => x < y case _ => false }) // https://www.scala-lang.org/old/node/7939
     .count(_ == true)
 
-val solve: Source => Int =
+val solveHalfway: Source => Int =
   parseInput andThen countDepthIncreases
+
+val solve: Source => Int =
+  parseInput andThen sumTriplets andThen countDepthIncreases
